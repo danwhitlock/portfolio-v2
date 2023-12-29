85d13a1ae9582b7263c3cca30f13db758e3cc187
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-12-29 17:18:13 +0000

    Implement Scroll Button JS

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const scrollToTop = document.querySelector(".scroll-to-top");
+
+scrollToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left:0,
+        behavior: "smooth",
+    })
+});
