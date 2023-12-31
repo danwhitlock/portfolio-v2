a7f9e311ef2392b73f23813a33f83363ae688ebf
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-12-31 12:14:07 +0000

    Build menu show/hide functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// Nav Menu (720px)
+
+const nav = document.querySelector("nav");
+const menuButton = document.querySelector(".menu-button");
+const menuUl = document.querySelector("nav ul");
+
+menuButton.addEventListener('click', () => {
+    menuUl.classList.toggle('show');
+})
+
+
+// Scroll to Top Button
+
 const scrollToTop = document.querySelector(".scroll-to-top");
 
 scrollToTop.addEventListener('click', () => {
